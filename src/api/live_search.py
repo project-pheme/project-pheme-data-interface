255ad3958d3bb17954838e7d6d23051fccac69e4
David Losada <david@tuxpiper.com>
2017-01-16 20:22:18 +0300

    live search: extract user name from raw json

--- src/api/live_search.py
+++ src/api/live_search.py
@@ -1,6 +1,6 @@
 from tornado_json import gen
 from tornado_json.requesthandlers import APIHandler
-import logging
+import logging, json
 
 import capture_api
 
@@ -14,4 +14,15 @@ class LiveSearchHandler(APIHandler):
     keywords = self.get_query_argument("keywords")
     max_results = self.get_query_argument("max_results") or 25
     results = yield capture_api.live_search(keywords=keywords.split(' '), max_results=max_results)
-    self.success(results)
+    # Function to post process results before returning to client
+    def process_result(result):
+      raw_json = result.pop("rawJson", None)
+      try:
+        if raw_json is not None:
+          raw_tweet = json.loads(raw_json)
+          result["userName"] = raw_tweet["user"]["name"]
+      except KeyError, e:
+        None
+      return result
+    ##
+    self.success(map(process_result, results))
